72c7f6fbb12ecdfd152e145d86c4cbd9339267f7
Andrés <andresfjz@hotmail.com>
2020-10-14 12:36:59 -0500

    Fuentes + login screen

--- app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
+++ app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
@@ -2,11 +2,15 @@ package senasoft2020.buhmed
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        textViewAppName.typeface = AppSenasoft2020.fontOpenSansRegular
+        textViewWelcome.typeface = AppSenasoft2020.fontOpenSansRegular
+        textViewMessage.typeface = AppSenasoft2020.fontOpenSansRegular
     }
 }
