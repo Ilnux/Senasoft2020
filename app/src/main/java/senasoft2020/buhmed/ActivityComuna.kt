9747da274ba55807fe5cd3992700c55f9b88aa30
Felipe Londoño <Felipe-0210@outlook.com>
2020-10-14 12:15:56 -0500

    Inicio proyecto

---
+++ app/src/main/java/senasoft2020/buhmed/ActivityComuna.kt
@@ -0,0 +1,11 @@
+package senasoft2020.buhmed
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityComuna : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comuna)
+    }
+}
--- app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
+++ app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
@@ -1,12 +1,56 @@
 package senasoft2020.buhmed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.huawei.hms.common.internal.HuaweiApiManager
+import com.huawei.hms.support.hwid.HuaweiIdAuthManager
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParams
+import com.huawei.hms.support.hwid.request.HuaweiIdAuthParamsHelper
+import com.huawei.hms.support.hwid.result.AuthHuaweiId
+import com.huawei.hms.support.hwid.service.HuaweiIdAuthService
 
 class LoginActivity : AppCompatActivity() {
+
+    val CODIGO = 9999
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
+
+
+    //Funciones Huawei
+    fun logHuawei() {
+        val solicitud: HuaweiIdAuthParams =
+            HuaweiIdAuthParamsHelper(HuaweiIdAuthParams.DEFAULT_AUTH_REQUEST_PARAM).setAuthorizationCode()
+                .setEmail().createParams()
+
+        val servicio:HuaweiIdAuthService = HuaweiIdAuthManager.getService(this,solicitud)
+
+        startActivityForResult(servicio.signInIntent,CODIGO)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CODIGO){
+            val tarea = HuaweiIdAuthManager.parseAuthResultFromIntent(data)
+            if(tarea.isSuccessful){
+                val cuenta = tarea.result
+                cambioAtividad(cuenta)
+            }
+        }
+    }
+
+
+    fun cambioAtividad(cuenta:AuthHuaweiId){
+        val intent = Intent(this,ActivityComuna::class.java)
+        intent.putExtra("cuenta", cuenta)
+        startActivity(intent)
+        finish()
+    }
 }
+
+
+
