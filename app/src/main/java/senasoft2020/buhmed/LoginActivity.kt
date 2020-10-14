d4bda7b9dda6cdf36008b7b654dba29d45a32dc6
Andrés <andresfjz@hotmail.com>
2020-10-14 11:37:39 -0500

    Splashscreen + cambio colores

--- app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
+++ app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
     }
