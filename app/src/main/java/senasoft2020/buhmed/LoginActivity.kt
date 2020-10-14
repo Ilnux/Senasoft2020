7eabe3c9c8edf0ccd713e840d5749b248f775a60
Andrés <andresfjz@hotmail.com>
2020-10-14 13:43:25 -0500

    123

--- app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
+++ app/src/main/java/senasoft2020/buhmed/LoginActivity.kt
@@ -1,5 +1,6 @@
 package senasoft2020.buhmed
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
@@ -9,8 +10,8 @@ class LoginActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        textViewAppName.typeface = AppSenasoft2020.fontOpenSansRegular
-        textViewWelcome.typeface = AppSenasoft2020.fontOpenSansRegular
-        textViewMessage.typeface = AppSenasoft2020.fontOpenSansRegular
+        buttonHuawei.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
