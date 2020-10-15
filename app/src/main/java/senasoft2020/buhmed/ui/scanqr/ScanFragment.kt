d1e8f0980570000965fbd82b2108929abc497bba
Andrés <andresfjz@hotmail.com>
2020-10-15 12:53:52 -0500

    Scan no finalizado

---
+++ app/src/main/java/senasoft2020/buhmed/ui/scanqr/ScanFragment.kt
@@ -0,0 +1,111 @@
+package senasoft2020.buhmed.ui.scanqr
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.os.Parcelable
+import android.text.TextUtils
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.huawei.hms.hmsscankit.ScanUtil
+import com.huawei.hms.ml.scan.HmsScan
+import com.huawei.hms.ml.scan.HmsScanAnalyzerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+import senasoft2020.buhmed.R
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [ScanFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class ScanFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_scan, container, false)
+        val buttonScan: Button = view.findViewById(R.id.buttonOpenScaner)
+        buttonScan.setOnClickListener{_ -> newViewBtnClick()}
+        return view
+    }
+
+    private fun newViewBtnClick() {
+        val list = arrayOf(Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(requireActivity(), list, DEFINED_CODE)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults.size < 2 || grantResults[0] != PackageManager.PERMISSION_GRANTED || grantResults[1] != PackageManager.PERMISSION_GRANTED) return
+        else if (requestCode == DEFINED_CODE) {
+            ScanUtil.startScan(activity, REQUEST_CODE_SCAN, HmsScanAnalyzerOptions.Creator().setHmsScanTypes(HmsScan.ALL_SCAN_TYPE).create())
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK || data == null) {return}
+        else if (requestCode == REQUEST_CODE_SCAN) {
+            when (val obj: Parcelable? = data.getParcelableExtra(ScanUtil.RESULT)) {
+                is HmsScan -> {
+                    if (!TextUtils.isEmpty(obj.getOriginalValue())) {
+                        Toast.makeText(activity, obj.getOriginalValue(), Toast.LENGTH_SHORT).show()
+                    }
+                    return
+                }
+            }
+        }
+    }
+
+    companion object {
+        private val DEFINED_CODE = 222
+        private val REQUEST_CODE_SCAN = 0X01
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment ScanFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            ScanFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
+    }
+}
